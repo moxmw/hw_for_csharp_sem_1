924c660e183c0e88ea4c29c4bba2b2cd78e8e411
Maximka <00_sip.papacy@icloud.com>
2023-07-04 23:41:34 +0400

    task_4 done

---
+++ task_4/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Clear();
+Console.WriteLine("Введите три числа:");
+    int num1 = int.Parse(Console.ReadLine());
+    int num2 = int.Parse(Console.ReadLine());
+    int num3 = int.Parse(Console.ReadLine());
+    int maxNum = Math.Max(Math.Max(num1, num2), num3);
+Console.WriteLine($"Максимальное число: {maxNum}");
